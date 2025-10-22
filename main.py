4a227e9104befb3131441a69afabe63b399bec60
Vadim <94108043+Showtimeeee@users.noreply.github.com>
2025-10-22 23:43:35 +0300

    Update main.py GUI

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from config import Config
 from src.actions import Actions
 from src.bot import Bot
+from src.gui import AFKBotGUI
 
 def main():
     config = Config()
     actions = Actions(config)
     bot = Bot(actions, config)
     
-    bot.start()
+    # Запускаем GUI
+    gui = AFKBotGUI(bot)
+    gui.run()
 
 if __name__ == "__main__":
     main()
